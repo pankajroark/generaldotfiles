0d571cf8bf8008ced4e0ddae31d69c1d0b801c3e
Pankaj Gupta <pankajroark@users.noreply.github.com>
2021-10-29 21:53:18 +0000

    bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 cp $(dirname $0)/dot_kubectlrc ~/dot_kubectlrc
+cp $(dirname $0)/dot_bashrc ~/dot_bashrc
 echo "source ~/dot_kubectlrc" >> ~/.bashrc
+echo "source ~/dot_bashrc" >> ~/.bashrc
+
