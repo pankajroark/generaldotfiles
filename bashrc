fd36ec9917159793fb94c01ebeb079f03e277617
Pankaj Gupta <pankaj.roark@gmail.com>
2021-10-30 04:08:54 -0700

    checkpoint

--- bashrc
+++
@@ -1,5 +0,0 @@
-pushd $(dirname $0)
-for f in $(ls ./into_bashrc); do
-  source ./into_bashrc/$f
-done
-popd
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,12 @@ pushd $(dirname $0)
 DOTDIR=""$HOME/.me""
 mkdir $DOTDIR
 cp -rf * $DOTDIR
-echo "source $DOTDIR/bashrc" >> ~/.bashrc
+
+pushd $DOTDIR
+for f in $(ls ./into_bashrc); do
+  echo "source $DOTDIR/into_bashrc/$f" >> ~/.bashrc
+done
+popd
 
 ln -s $DOTDIR/vimrc ~/.vimrc 
 ln -s $DOTDIR/gitconfig ~/.gitconfig 
