ccf7b0b3a976787b2b9650a31e463088aad682e8
Pankaj Gupta <pankaj.roark@gmail.com>
2021-11-06 06:50:25 -0700

    checkpoint

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,17 @@
 pushd $(dirname $0)
 # This should match the one in aliases
 DOTDIR="$HOME/.me"
+rm -rf $DOTDIR
 mkdir $DOTDIR
 cp -rf * $DOTDIR
 
 pushd $DOTDIR
+> ~/.my_bashrc
 for f in $(ls ./into_bashrc); do
-  echo "source $DOTDIR/into_bashrc/$f" >> ~/.bashrc
+  echo "source $DOTDIR/into_bashrc/$f" >> ~/.my_bashrc
 done
+
+grep -qxF "source ~/.my_bashrc" ~/.bashrc || echo "source ~/.my_bashrc" >> ~/.bashrc
 popd
 
 ln -s $DOTDIR/vimrc ~/.vimrc 
